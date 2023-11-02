8e3eebe2adbe846e8453dcc2b008e00c71db9bd8
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-11-02 22:09:13 +0530

    image picker added

--- lib/widgets/methods.dart
+++ lib/widgets/methods.dart
@@ -11,6 +11,12 @@ callTextField(
     required TextEditingController inputcontroller,
     required int max}) {
   return TextFormField(
+    validator: (value) {
+      if (value == null || value.isEmpty) {
+        return 'This is a required field';
+      }
+      return null;
+    },
     maxLines: max,
     controller: inputcontroller,
     decoration: InputDecoration(
@@ -23,13 +29,13 @@ callTextField(
 }
 
 TextStyle textStyle(
-    FontWeight? fontWeight,
-    double? fontSize,
-    Color? color,
-  ) {
-    return TextStyle(
-      fontWeight: fontWeight,
-      fontSize: fontSize,
-      color: color,
-    );
-  }
+  FontWeight? fontWeight,
+  double? fontSize,
+  Color? color,
+) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: fontSize,
+    color: color,
+  );
+}
